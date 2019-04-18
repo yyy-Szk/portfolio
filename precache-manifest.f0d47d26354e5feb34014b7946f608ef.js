30387c11b8b2db2cf9e491f862cb4ec731d90ed4
Yoshimasa Suzuki <cognac0004@gmail.com>
2019-04-19 08:10:26 +0900

    Updates

---
+++ precache-manifest.f0d47d26354e5feb34014b7946f608ef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "14ca52d34905191ad808",
+    "url": "/portfolio/static/css/main.3757c381.chunk.css"
+  },
+  {
+    "revision": "14ca52d34905191ad808",
+    "url": "/portfolio/static/js/main.27ea72f8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bc9e213220f85ecf4864",
+    "url": "/portfolio/static/js/2.501d70a8.chunk.js"
+  },
+  {
+    "revision": "4fa83c48f12d491ce67b39045a1d362d",
+    "url": "/portfolio/static/media/yoshimasa_suzuki.4fa83c48.jpg"
+  },
+  {
+    "revision": "9bea711ce70c101011a10aba5d09d3f8",
+    "url": "/portfolio/static/media/hair-more.9bea711c.png"
+  },
+  {
+    "revision": "41f6a2673fb6743b37b5741e3a265927",
+    "url": "/portfolio/static/media/twitter.41f6a267.jpg"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/portfolio/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "fbe01551b3091103d2655e6be9d8cd9f",
+    "url": "/portfolio/static/media/css3.fbe01551.png"
+  },
+  {
+    "revision": "893d98e016e391cfcdc9fd77c7b621dc",
+    "url": "/portfolio/static/media/javascript.893d98e0.svg"
+  },
+  {
+    "revision": "7f52234e4ca6236e21492ea52b00e535",
+    "url": "/portfolio/static/media/ruby.7f52234e.jpeg"
+  },
+  {
+    "revision": "9999c1a8c347b81c5cb5df967664c4c3",
+    "url": "/portfolio/static/media/jquery.9999c1a8.gif"
+  },
+  {
+    "revision": "d14e4fd3c8b856f05612643d6773e497",
+    "url": "/portfolio/static/media/react.d14e4fd3.png"
+  },
+  {
+    "revision": "4b5633956a610c180e37c8fd3d971c8e",
+    "url": "/portfolio/static/media/rails.4b563395.png"
+  },
+  {
+    "revision": "efe824a9228b3a601b17ad070bc91f73",
+    "url": "/portfolio/index.html"
+  }
+];
