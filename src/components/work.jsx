f73b5198e20c666fa5afe6354d01da8d81a3c288
Yoshimasa Suzuki <cognac0004@gmail.com>
2019-05-06 06:38:49 +0900

    fixed work.jsx

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -15,12 +15,15 @@ class Work extends React.Component {
           <Grid container className="container">
             <Grid item xs={12} className="works image_box">
               <img src={personal_diary} alt="personal_diary"/>
-              <a href="https://github.com/Yoshimasa-Suzuki/gucchier">github</a> / <a href="https://polar-retreat-48576.herokuapp.com/">heroku</a>
+              <a href="https://github.com/Yoshimasa-Suzuki/gucchier" target="_blank" rel="noopener noreferrer">github</a>
+              /
+              <a href="https://polar-retreat-48576.herokuapp.com/" target="_blank" rel="noopener noreferrer">heroku</a>
             </Grid>
             <div class="desc">
               <p>　１日ごとにリセットされるメモの機能とその日の出来事をまとめておく日記を書くというシンプルなサービス</p>
               <p>　業務のなかで学んだReactをフロントエンドに、Railsをバックエンドに使用して、別々のサーバーで運用してみたいと思い作成しました。</p>
-              <p>　deviseを用いたtoken認証やフロントとバックを完全分離して開発することなど、使ってみたい技術を使う意識で開発したため、リファクタリングやテスト、デザインなどは甘くなっています。</p>
+              <p>　deviseを用いたtoken認証やフロントとバックを完全分離して開発することなど、使ってみたい技術を使う意識で開発したため、
+              リファクタリングやテスト、デザインなどは甘くなっています。</p>
             </div>
           </Grid>
         </div>
@@ -31,12 +34,16 @@ class Work extends React.Component {
           <Grid contaier className="container" style={{textAlign: 'center'}}>
             <Grid item xs={12} className="works">
               <img src={gucchier} alt="gucchier"/>
-              <a href="https://github.com/Yoshimasa-Suzuki/gucchier">github</a> / <a href="https://polar-retreat-48576.herokuapp.com/">heroku</a>
+              <a href="https://github.com/Yoshimasa-Suzuki/gucchier" target="_blank" rel="noopener noreferrer">github</a>
+              /
+              <a href="https://polar-retreat-48576.herokuapp.com/" target="_blank" rel="noopener noreferrer">heroku</a>
             </Grid>
 
             <Grid item xs={12} className="works">
               <img src={hair_more} alt="hair-more"/>
-              <a href="https://github.com/Yoshimasa-Suzuki/hair-more">github</a> / <a href="https://mighty-journey-43203.herokuapp.com/">heroku</a>
+              <a href="https://github.com/Yoshimasa-Suzuki/hair-more" target="_blank" rel="noopener noreferrer">github</a>
+              /
+              <a href="https://mighty-journey-43203.herokuapp.com/" target="_blank" rel="noopener noreferrer">heroku</a>
             </Grid>
           </Grid>
         </div>
